8d9c0887d16d5a70f69bda308fff6fb33ffa57db
fullth <lth129@naver.com>
2021-12-30 10:42:09 +0900

    ✒ DB 연동 관련 소스 제거

--- src/test/java/com/tistory/fullth/FullthApplicationTests.java
+++ src/test/java/com/tistory/fullth/FullthApplicationTests.java
@@ -2,15 +2,16 @@ package com.tistory.fullth;
 
 import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 class FullthApplicationTests {
 
-    /*
     @Test
-    @Ignore
     void contextLoads() {
     }
-    */
+
 }
