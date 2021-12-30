48fc1bb6e7d9f6e9ef545c24874b0fcce6698218
fullth <lth129@naver.com>
2021-12-30 10:10:15 +0900

    ✒ Update FullthApplicationTests: contextLoads() 미사용

--- src/test/java/com/tistory/fullth/FullthApplicationTests.java
+++ src/test/java/com/tistory/fullth/FullthApplicationTests.java
@@ -1,13 +1,16 @@
 package com.tistory.fullth;
 
+import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FullthApplicationTests {
 
+    /*
     @Test
+    @Ignore
     void contextLoads() {
     }
-
+    */
 }
